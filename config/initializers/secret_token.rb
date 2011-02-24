b4a8f80ecc2b6ca1a70815b65aebf60d9e3202e3
qavvap <qavvap@googlemail.com>
2011-02-24 22:47:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kl2011::Application.config.secret_token = '93687b0967e296b86ce226a818eec2862c1f96a2467e6be89cd19a32d90ba39cfaa1971cdabfc3c3965eb670a658d45342952ceae9ba0378bdc27d0b5d4c6e69'
