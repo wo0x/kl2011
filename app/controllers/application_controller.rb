fc6700365ef0fdeeb692114735f59738fc17d662
qavvap <qavvap@googlemail.com>
2011-02-27 23:10:11 +0100

    installed langswitch via subdomain, including langswitch_linkto_helper. need some more beauty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
   def set_locale
-    I18n.locale = params[:locale] 
+    I18n.locale =  extract_locale_from_subdomain
   end
 
+
+private
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 module ApplicationHelper
 
+  def langswitch_linkto_helper(to_lang)
+    "http://#{to_lang}.app.com:#{request.env['SERVER_PORT']}#{request.env['PATH_INFO']}"
+  end 
 end
